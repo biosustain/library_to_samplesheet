08cdcac2b41413ea9deb0ba63710b58de5aeee73
meono <emoz@biosustain.dtu.dk>
2019-10-08 16:27:11 +0200

    minimal working version

--- library_to_samplesheet/library_to_samplesheet.py
+++ library_to_samplesheet/library_to_samplesheet.py
@@ -4,6 +4,7 @@
 
 from xml.etree import ElementTree
 from library_to_samplesheet.adapters import adapters
+from click import BadParameter
 
 
 def parse_run_parameters(file_path: str) -> list:
@@ -14,13 +15,18 @@ def parse_run_parameters(file_path: str) -> list:
     :return: list with header title and read lengths.
     """
 
-    run_paramters = ElementTree.parse(file_path)
-    read_lengts = list()
+    run_parameters = ElementTree.parse(file_path)
+    read_lengths = list()
     for param in ['Setup/Read1', 'Setup/Read2']:
-        if run_paramters.find(param):
-            read_lengts.append(run_paramters.find(param).text)
+        if run_parameters.find(param) is not None:
+            read_lengths.append(run_parameters.find(param).text)
 
-    return ['[Reads]'] + read_lengts
+    # check if read lengths are collected, fail if not.
+    if len(read_lengths) == 0:
+        raise BadParameter('RunParamter.xml does not contain expected read '
+                           'length branch.')
+
+    return ['[Reads]'] + read_lengths
 
 
 def parse_library_sheet(file_path: str) -> dict:
@@ -46,13 +52,14 @@ def parse_library_sheet(file_path: str) -> dict:
                 segments[segment_title].append(line)
 
     # put header segment in dictionary
-    segments['[Header]'] = {param.split(',')[0]: param.split(',')[1]
+    segments['[Header]'] = {param.split(',')[0]:
+                                (param.split(',')[1] if ',' in param else '')
                             for param in segments['[Header]']}
 
     return segments
 
 
-def write_sample_sheet(file_path: str, run_paramters: dict, library: dict):
+def write_sample_sheet(file_path: str, run_parameters: list, library: dict):
     """
     Generates sample sheet to be used with bcl2fastq.
     :param file_path: destination and name for the sample sheet.
@@ -62,7 +69,7 @@ def write_sample_sheet(file_path: str, run_paramters: dict, library: dict):
 
     with open(file_path, 'w') as file:
         # write read lengths
-        file.writelines([param+'\n' for param in run_paramters])
+        file.writelines([param + '\n' for param in run_parameters])
 
         # write adapters
         library_prep_kit = library['[Header]']['LibraryPrepKit']
@@ -73,24 +80,55 @@ def write_sample_sheet(file_path: str, run_paramters: dict, library: dict):
 
         # adapt and write library data
         index = library['[Data]'][0].split(',').index('Index2Sequence')
-        complement = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A'}
 
         data = ['[Data]\n'] + \
-               [library['[Data]'][0].replace('SampleID', 'Sample_ID')
-                                    .replace('Name', 'Sample_Name')
-                                    .replace('Well', 'Sample_Well')
-                                    .replace('Index1Name', 'I7_Index_ID')
-                                    .replace('Index1Sequence', 'index')
-                                    .replace('Index2Name', 'I5_Index_ID')
-                                    .replace('Index2Sequence', 'index2')
-                                    .replace('Project', 'Sample_Project') +
-                '\n'] + \
-               [','.join(sample[0:index] +
-                         ["".join(complement.get(base, base)
-                                  for base in reversed(sample[9]))] +
-                         sample[index+1:]) for sample in library['[Data]'][1:]]
+               [adjust_data_header(library['[Data]'][0]) + '\n'] + \
+               [adjust_sample(sample, index) + '\n'
+                for sample in library['[Data]'][1:]]
         file.writelines(data)
 
     return
 
 
+def adjust_data_header(header: str) -> str:
+    """
+    Returns data header with adjusted column names.
+    :param header: data header string
+    :return: adjusted data header string
+    """
+    replacements = {'SampleID': 'Sample_ID',
+                    'Name': 'Sample_Name',
+                    'Well': 'Sample_Well',
+                    'Index1Name': 'I7_Index_ID',
+                    'Index1Sequence': 'index',
+                    'Index2Name': 'I5_Index_ID',
+                    'Index2Sequence': 'index2',
+                    'Project': 'Sample_Project'
+                    }
+    return ','.join([replacements[col] if col in replacements else col
+                       for col in header.split(',')])
+
+
+
+def adjust_sample(sample: str, index: int) -> str:
+    """
+    Takes in a sample line from library sheet anc adjust for use in samplesheet.
+    :param sample: sample line from library sheet
+    :param index: list index for Index2Sequence
+    :return: sample line suitable for samplesheet
+    """
+
+    sample = sample.split(',')
+    return ','.join(sample[0:index] +
+                    [reverse_complement(sample[index])] + sample[index + 1:])
+
+
+def reverse_complement(sequence: str) -> str:
+    """
+    A simple function that returns a reverse complement of a give DNA (ATGC)
+    sequence.
+    :param sequence: all capital DNA sequence as string
+    :return: reverse complemented sequence
+    """
+    complement = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A'}
+    return "".join([complement.get(base, base) for base in reversed(sequence)])
